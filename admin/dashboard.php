7bd23ecf5beff241a737baebdada5297bc9daee6
VThanhDat <121113462+VThanhDat@users.noreply.github.com>
2024-10-30 01:05:24 +0700

    update Dashboard admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -23,7 +23,9 @@ if (strlen($_SESSION['alogin']) == 0) {
     </head>
 
     <body>
+        <!------MENU SECTION START-->
         <?php include('includes/header.php'); ?>
+        <!-- MENU SECTION END-->
         <div class="content-wrapper">
             <div class="container">
                 <div class="row pad-botm">
@@ -31,21 +33,137 @@ if (strlen($_SESSION['alogin']) == 0) {
                         <h4 class="header-line">TRANG QUẢN LÝ</h4>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-success back-widget-set text-center">
+                            <i class="fa fa-book fa-5x"></i>
+                            <!-- hàm đếm sách -->
+                            <?php
+                            $sql = "SELECT id from sach";
+                            $query = $dbh->prepare($sql);
+                            $query->execute();
+                            $results = $query->fetchAll(PDO::FETCH_OBJ);
+                            $listdbooks = $query->rowCount();
+                            ?>
+                            <h3><?php echo htmlentities($listdbooks); ?></h3>
+                            Số sách
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-info back-widget-set text-center">
+                        <i class="fa fa-bars fa-5x"></i>
 
 
+                        <?php
+                        $sql1 = "SELECT id from ctmuontra ";
+                        $query1 = $dbh->prepare($sql1);
+                        $query1->execute();
+                        $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+                        $issuedbooks = $query1->rowCount();
+                        ?>
 
+                        <h3><?php echo htmlentities($issuedbooks); ?> </h3>
+                        Số sách đã được mượn
+                        </div>x
+                    </div>
 
-                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-                <!-- CONTENT-WRAPPER SECTION END-->
-                <?php include('includes/footer.php'); ?>
-                <!-- FOOTER SECTION END-->
-                <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-                <!-- CORE JQUERY  -->
-                <script src="assets/js/jquery-1.10.2.js"></script>
-                <!-- BOOTSTRAP SCRIPTS  -->
-                <script src="assets/js/bootstrap.js"></script>
-                <!-- CUSTOM SCRIPTS  -->
-                <script src="assets/js/custom.js"></script>
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-warning back-widget-set text-center">
+                        <i class="fa fa-recycle fa-5x"></i>
+                        <?php
+                            $status = 2;
+                            $sql2 = "SELECT id from ctmuontra where BorrowStatus=:status";
+                            $query2 = $dbh->prepare($sql2);
+                            $query2->bindParam(':status', $status, PDO::PARAM_STR);
+                            $query2->execute();
+                            $results2 = $query2->fetchAll(PDO::FETCH_OBJ);
+                            $returnedbooks = $query2->rowCount();
+                        ?>
+                        <h3><?php echo htmlentities($returnedbooks); ?></h3>
+                        Số sách đã được trả
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-danger back-widget-set text-center">
+                        <i class="fa fa-users fa-5x"></i>
+                        <?php
+                            $sql3 = "SELECT id from docgia ";
+                            $query3 = $dbh->prepare($sql3);
+                            $query3->execute();
+                            $results3 = $query3->fetchAll(PDO::FETCH_OBJ);
+                            $regstds = $query3->rowCount();
+                        ?>
+                        <h3><?php echo htmlentities($regstds); ?></h3>
+                        Số lượng người dùng
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-success back-widget-set text-center">
+                        <i class="fa fa-user fa-5x"></i>
+                        <?php
+                            $sql4 = "SELECT id from tacgia ";
+                            $query4 = $dbh->prepare($sql4);
+                            $query4->execute();
+                            $results4 = $query4->fetchAll(PDO::FETCH_OBJ);
+                            $listdathrs = $query4->rowCount();
+                        ?>
+                        <h3><?php echo htmlentities($listdathrs); ?></h3>
+                        Số lượng tác giả
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-3 rscol-xs-6">
+                        <div class="alert alert-info back-widget-set text-center">
+                        <i class="fa fa-file-archive-o fa-5x"></i>
+                        <?php
+                            $sql5 = "SELECT id from theloai ";
+                            $query5 = $dbh->prepare($sql5);
+                            $query5->execute();
+                            $results5 = $query5->fetchAll(PDO::FETCH_OBJ);
+                            $listdcats = $query5->rowCount();
+                        ?>
+
+                        <h3><?php echo htmlentities($listdcats); ?> </h3>
+                        Số thể loại
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-warning back-widget-set text-center">
+                        <i class="fa fa-recycle fa-5x"></i>
+                        <?php
+                            $status = 1;
+                            $stmt1 = $dbh->prepare("CALL LaySachDaMuon(:status)");
+                            $query2->bindParam(':status', $status, PDO::PARAM_STR);
+                            $query2->execute();
+                            $results2 = $query2->fetchAll(PDO::FETCH_OBJ);
+                            $returnedbooks = $query2->rowCount();            
+                        ?>
+                        <h3><?php echo htmlentities($returnedbooks); ?></h3>
+                        Số sách đã được chưa trả
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- CONTENT-WRAPPER SECTION END-->
+        <?php include('includes/footer.php'); ?>
+        <!-- FOOTER SECTION END-->
+        <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+        <!-- CORE JQUERY  -->
+        <script src="assets/js/jquery-1.10.2.js"></script>
+        <!-- BOOTSTRAP SCRIPTS  -->
+        <script src="assets/js/bootstrap.js"></script>
+        <!-- DATATABLE SCRIPTS  -->
+        <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+        <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+        <!-- CUSTOM SCRIPTS  -->
+        <script src="assets/js/custom.js"></script>
     </body>
 
     </html>
